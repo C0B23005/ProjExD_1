7a51dd6f57dac52fe97661d0c29ecd2c8df6e4d0
abe <c0b23005b0@edu.teu.ac.jp>
2024-09-24 17:41:40 +0900

    2

--- pygame_image.py
+++ pygame_image.py
@@ -20,19 +20,32 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        kk_rct.move_ip((-1, 0))
+
+        
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            # kk_rct.move_ip((0, -1))
+            Y = -1
            
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
-            
+        elif key_lst[pg.K_DOWN]:
+            # kk_rct.move_ip((0, +1))
+            Y = +1
+
+        else:
+            Y = 0
+
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            # kk_rct.move_ip((-1, 0))
+            X = -2
             
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+2, 0))
+        elif key_lst[pg.K_RIGHT]:
+            # kk_rct.move_ip((+2, 0))
+            X = +2
+
+        else:
+            X = -1
+
+        kk_rct.move_ip((X, Y))
             
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
